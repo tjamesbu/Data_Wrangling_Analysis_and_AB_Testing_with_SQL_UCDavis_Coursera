03cbd75b8f57796f4f54b79bccd494185fb94d3f
tjamesbu <64707274+tjamesbu@users.noreply.github.com>
2021-04-26 20:16:01 -0500

    Create Q5.sql

---
+++ AB_Testing_Assignment/Q5.sql
@@ -0,0 +1,6 @@
+--Use the https://thumbtack.github.io/abba/demo/abba.html to compute the lifts in metrics and the p-values for the binary metrics ( 30 day order binary and 30 day view binary) using a interval 95% confidence. 
+
+  
+--For orders_bin: lift is -15% – 11% (-2.2%) and pval is 0.74
+--For views_bin: lift is and pval is -2.1% – 5.9% (1.9%) and pval is 0.36
+--Therefore for item_test_2, there was no significant difference in either the number of views or the number of orders between control and experiment
